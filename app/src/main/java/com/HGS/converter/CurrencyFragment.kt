44faecbab2ead15b75e022c35d223aec7403fda5
William Liu <williamliu8668@gmail.com>
2019-04-30 13:24:16 +0800

    add fragment map

---
+++ app/src/main/java/com/HGS/converter/CurrencyFragment.kt
@@ -0,0 +1,14 @@
+package com.HGS.converter
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CurrencyFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_currency, container,false)
+    }
+
+}
--- app/src/main/java/com/HGS/converter/MainActivity.kt
+++ app/src/main/java/com/HGS/converter/MainActivity.kt
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    val fragments = mapOf(R.id.temperature to TemperatureFragment(),
+                         R.id.volume to VolumeFragment(),
+                         R.id.weight to WeightFragment(),
+                         R.id.currency to CurrencyFragment()
+                        )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
