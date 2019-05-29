820a28fe8b5a978c82babacfd6d20ffbc78fd65c
William Liu <williamliu8668@gmail.com>
2019-05-28 23:21:48 -0700

    Make two spinners Items & onItemSelectedListener
    the first spinner has C & F items
    when you select one of them, the lowest text will show the same string

--- app/src/main/java/com/HGS/converter/TemperatureFragment.kt
+++ app/src/main/java/com/HGS/converter/TemperatureFragment.kt
@@ -8,6 +8,9 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import kotlinx.android.synthetic.main.fragment_temperature.*
 
 class TemperatureFragment : Fragment() {
@@ -25,6 +28,30 @@ class TemperatureFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        val Inputspinner:Spinner = tempInputSpinner
+
+        ArrayAdapter.createFromResource(
+            this.activity,
+            R.array.temItem_Array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            Inputspinner.adapter = adapter
+        }
+
+        Inputspinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                ID_c2F.text = Inputspinner.getSelectedItem().toString()
+            }
+
+        }
+
+
 
         tempFText.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
